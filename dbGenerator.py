ee20e56c0cc90659b301b713658de483ea864df3
Jordi Serrano <44474715+j0rd1s3rr4n0@users.noreply.github.com>
2023-05-13 23:05:02 +0200

    .

--- dbGenerator.py
+++ dbGenerator.py
@@ -1,57 +1,58 @@
 import sqlite3
+import hashlib
+import random
 
 database = "database.db"
 
 def dropTable(tablename):
     conn = sqlite3.connect(database)
     cursor = conn.cursor()
-    cursor.execute("TRUNCATE TABLE IF EXISTS "+tablename+" ;")
+    cursor.execute("DELETE FROM "+tablename+" ;")
     conn.commit()
     conn.close()
+
 
 def GenerateDataBases():
     conn = sqlite3.connect(database)
     cursor = conn.cursor()
 
     db_airport_drop = "DROP TABLE IF EXISTS airports;"
-    db_kahoot_drop = "DROP TABLE IF EXISTS preguntas;"
-    db_users_drop = "DROP TABLE IF EXISTS usuarios;"
+    db_kahoot_drop = "DROP TABLE IF EXISTS questions;"
+    db_users_drop = "DROP TABLE IF EXISTS users;"
     db_leaderboard_drop = "DROP TABLE IF EXISTS leaderboard_table;"
-    db_incidencias_drop = "DROP TABLE IF EXISTS incidencias;"
-    db_passagers_drop = "DROP TABLE IF EXISTS pasajeros;"
+    db_incidences_drop = "DROP TABLE IF EXISTS incidents;"
+    db_passengers_drop = "DROP TABLE IF EXISTS passengers;"
 
     db_airport_create = "CREATE TABLE IF NOT EXISTS airports (code TEXT PRIMARY KEY, lat TEXT, lon TEXT, name TEXT, city TEXT, state TEXT, country TEXT, woeid TEXT, tz TEXT, phone TEXT, type TEXT, email TEXT, url TEXT, runway_length TEXT, elev TEXT, icao TEXT, direct_flights TEXT, carriers TEXT);"
-    db_kahoot_create = "CREATE TABLE preguntas (id INTEGER PRIMARY KEY AUTOINCREMENT, question TEXT NOT NULL, opcion_a TEXT NOT NULL, opcion_b TEXT NOT NULL, opcion_c TEXT NOT NULL, opcion_d TEXT NOT NULL, respuesta TEXT NOT NULL);"
-    db_users_create = "CREATE TABLE IF NOT EXISTS usuarios (id INTEGER PRIMARY KEY AUTOINCREMENT, usuario TEXT NOT NULL, contrasena TEXT NOT NULL, apikey TEXT NOT NULL)"
-    db_leaderboard = "CREATE TABLE IF NOT EXISTS leaderboard_table (id INTEGER PRIMARY KEY, nickname TEXT, asiento INTEGER, puntos INTEGER, FOREIGN KEY (asiento) REFERENCES pasajeros (seat));"
-    db_incidencias = "CREATE TABLE IF NOT EXISTS incidencias ( id INTEGER PRIMARY KEY, seat TEXT NOT NULL, type TEXT NOT NULL, comments TEXT, active INTEGER DEFAULT 1, timestamp_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP, timestamp_updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP, FOREIGN KEY (seat) REFERENCES pasajeros (seat));"
-    db_passagers = "CREATE TABLE IF NOT EXISTS pasajeros (seat TEXT PRIMARY KEY, name TEXT NOT NULL, surnames TEXT NOT NULL, id_card TEXT NOT NULL, email TEXT NOT NULL);"
-
-
-
+    db_kahoot_create = "CREATE TABLE IF NOT EXISTS questions (id INTEGER PRIMARY KEY AUTOINCREMENT, question TEXT NOT NULL, option_a TEXT NOT NULL, option_b TEXT NOT NULL, option_c TEXT NOT NULL, option_d TEXT NOT NULL, answer TEXT NOT NULL);"
+    db_users_create = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, user TEXT NOT NULL, password TEXT NOT NULL, apikey TEXT NOT NULL);"
+    db_leaderboard = "CREATE TABLE IF NOT EXISTS leaderboard_table (id INTEGER PRIMARY KEY, nickname TEXT, seat INTEGER, points INTEGER, FOREIGN KEY (seat) REFERENCES passengers (seat));"
+    db_incidents = "CREATE TABLE IF NOT EXISTS incidents (id INTEGER PRIMARY KEY, seat TEXT NOT NULL, type TEXT NOT NULL, comments TEXT, active INTEGER DEFAULT 1, timestamp_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP, timestamp_updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP, FOREIGN KEY (seat) REFERENCES passengers (seat));"
+    db_passengers = "CREATE TABLE IF NOT EXISTS passengers (seat TEXT PRIMARY KEY, name TEXT NOT NULL, surnames TEXT NOT NULL, id_card TEXT NOT NULL, email TEXT NOT NULL);"
 
     cursor.execute(db_airport_drop)
     cursor.execute(db_kahoot_drop)
     cursor.execute(db_users_drop)
     cursor.execute(db_leaderboard_drop)
-    cursor.execute(db_incidencias_drop)
-    cursor.execute(db_passagers_drop)
+    cursor.execute(db_incidences_drop)
+    cursor.execute(db_passengers_drop)
 
     cursor.execute(db_airport_create)
     cursor.execute(db_kahoot_create)
     cursor.execute(db_users_create)
     cursor.execute(db_leaderboard)
-    cursor.execute(db_incidencias)
-    cursor.execute(db_passagers)
+    cursor.execute(db_incidents)
+    cursor.execute(db_passengers)
 
     conn.commit()
     conn.close()
 
-def insertKahootQuest(pregunta, opciones):
-    """Inserta una pregunta y sus opciones en la tabla 'preguntas' de la base de datos"""
+
+def insertKahootQuest(question, options):
+    """Inserts a question and its options in the 'questions' table of the database"""
     conn = sqlite3.connect(database)
     cursor = conn.cursor()
-    cursor.execute('''INSERT INTO preguntas (question, opcion_a, opcion_b, opcion_c, opcion_d, respuesta) VALUES (?, ?, ?, ?, ?, ?)''', (pregunta, opciones[0], opciones[1], opciones[2], opciones[3], opciones[4]))
+    cursor.execute('''INSERT INTO questions (question, option_a, option_b, option_c, option_d, answer) VALUES (?, ?, ?, ?, ?, ?)''', (question, options[0], options[1], options[2], options[3], options[4]))
     conn.commit()
     conn.close()
 
@@ -63,44 +64,47 @@ def insertAirportsData():
     conn.commit()
     conn.close()
 
-def insertPasajerosData():
+def insertPasajerosData(seat,name,surnames,id_card,email):
     conn = sqlite3.connect(database)
     cursor = conn.cursor()
-    sql = "INSERT INTO pasajeros (seat, name, surnames, id_card, email) VALUES (?, ?, ?, ?, ?);"
-    values = ("1A", "Juan", "Pérez Sánchez", "12345678A", "juanperez@example.com")
+    sql = "INSERT INTO passengers (seat, name, surnames, id_card, email) VALUES (?, ?, ?, ?, ?);"
+    values = (seat,name,surnames,id_card,email)
     cursor.execute(sql, values)
     conn.commit()
     conn.close()
 
-def insertUserData():
+
+def insertUserData(user,password):
+    password = hashlib.sha256(b''+password+'').hexdigest()
+    apikey = hashlib.sha256(b''+password+'').hexdigest()
     conn = sqlite3.connect(database)
     cursor = conn.cursor()
-    sql = "INSERT INTO usuarios (usuario, contrasena, apikey) VALUES ('user1', 'pass1', 'key1');"
+    sql = f"INSERT INTO users (user, password, apikey) VALUES ('{user}', '{password}', '{apikey}');"
     cursor.execute(sql)
     conn.commit()
     conn.close()
 
-def insertLeaderboardData(seat,pts,nickname):
+def insertLeaderboardData(nickname,seat,pts):
     conn = sqlite3.connect(database)
     cursor = conn.cursor()
-    sql = "INSERT INTO leaderboard_table (nickname, asiento, puntos) VALUES ('?', '?', ?);", (nickname, seat, pts)
+    sql = "INSERT INTO leaderboard_table (nickname, seat, points) VALUES (?, ?, ?);"
+    cursor.execute(sql, (nickname, seat, pts))
+    conn.commit()
+    conn.close()
+
+def insertIncidenciasData(seat,type,comments="No Comments"):
+    conn = sqlite3.connect(database)
+    cursor = conn.cursor()
+    sql = f"INSERT INTO incidents (seat, type, comments, active) VALUES ('{seat}', '{type}', '{comments}', 1);"
     cursor.execute(sql)
     conn.commit()
     conn.close()
 
-def insertIncidenciasData():
+
+def updatePassagersData(seat,name,surname,id_card,email):
     conn = sqlite3.connect(database)
     cursor = conn.cursor()
-    sql = "INSERT INTO incidences (seat, type, comments, active) VALUES ('1A', 'delay', 'Flight delayed 2 hours', 1);"
-    cursor.execute(sql)
-    conn.commit()
-    conn.close()
-
-
-def insertPasajerosData():
-    conn = sqlite3.connect(database)
-    cursor = conn.cursor()
-    sql = "INSERT INTO pasajeros (seat, name, surnames, id_card, email) VALUES ('1A', 'John', 'Doe', '12345678A', 'johndoe@example.com');"
+    sql = "UPDATE passengers SET email = ? WHERE id = ?;"
     cursor.execute(sql)
     conn.commit()
     conn.close()
@@ -109,7 +113,7 @@ def insertPasajerosData():
 def getLeaderboard():
     conn = sqlite3.connect(database)
     cursor = conn.cursor()
-    sql = "SELECT * FROM leaderboard_table ORDER BY puntos DESC;"
+    sql = "SELECT * FROM leaderboard_table ORDER BY points DESC LIMIT 50;"
     cursor.execute(sql)
     result = cursor.fetchall()
     conn.close()
@@ -127,7 +131,7 @@ def updateAirportsData(code, name):
 def updateUsersData(id, username, password):
     conn = sqlite3.connect(database)
     cursor = conn.cursor()
-    sql = "UPDATE usuarios SET usuario = ?, contrasena = ? WHERE id = ?;"
+    sql = "UPDATE users SET username = ?, password = ? WHERE id = ?;"
     cursor.execute(sql, (username, password, id))
     conn.commit()
     conn.close()
@@ -135,7 +139,7 @@ def updateUsersData(id, username, password):
 def updateLeaderboardData(id, points):
     conn = sqlite3.connect(database)
     cursor = conn.cursor()
-    sql = "UPDATE leaderboard_table SET puntos = ? WHERE id = ?;"
+    sql = "UPDATE leaderboard_table SET points = ? WHERE id = ?;"
     cursor.execute(sql, (points, id))
     conn.commit()
     conn.close()
@@ -143,7 +147,7 @@ def updateLeaderboardData(id, points):
 def updateIncidencesData(id, comments):
     conn = sqlite3.connect(database)
     cursor = conn.cursor()
-    sql = "UPDATE incidences SET comments = ? WHERE id = ?;"
+    sql = "UPDATE incidents SET comments = ? WHERE id = ?;"
     cursor.execute(sql, (comments, id))
     conn.commit()
     conn.close()
@@ -151,23 +155,16 @@ def updateIncidencesData(id, comments):
 def updatePassagersData(id, email):
     conn = sqlite3.connect(database)
     cursor = conn.cursor()
-    sql = "UPDATE pasajeros SET email = ? WHERE id = ?;"
+    sql = "UPDATE passengers SET email = ? WHERE id = ?;"
     cursor.execute(sql, (email, id))
     conn.commit()
     conn.close()
 
 
-
-
-
-
-
-
-
-def get_random_questions(num_preguntas=10):
+def get_random_questions(num_questions=10):
     conn = sqlite3.connect(database)
     cursor = conn.cursor()
-    cursor.execute(f'SELECT * FROM preguntas ORDER BY RANDOM() LIMIT {num_preguntas};')
+    cursor.execute(f'SELECT * FROM questions ORDER BY RANDOM() LIMIT {num_questions};')
     preguntas = []
     for row in cursor.fetchall():
         pregunta = {
--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import requests
 import threading
 from flask import Flask, request as rr
 import sqlite3
-#files
+# files
 import dbGenerator as db
 
 # OPENAI DATA
@@ -19,20 +19,19 @@ openai.api_base = "https://openaipasionaus.openai.azure.com/"
 openai.api_version = "2022-12-01"
 ENGINE_MODEL = "textDavinci03Model"
 
-# VARIABLES VUELO
+# FLIGHT VARIABLES
 origin_city = ""
 destination_city = ""
 origin_date = ""
 destination_date = ""
 
 
-# Define el decorador
+# Define the decorator
 def add_cors_headers(response):
     response.headers['Access-Control-Allow-Origin'] = '*'
     response.headers['Access-Control-Allow-Headers'] = 'Content-Type,Authorization'
     response.headers['Access-Control-Allow-Methods'] = 'GET,PUT,POST,DELETE,OPTIONS'
     return response
-
 
 def OpenAiReConfig(API_TYPE_NEW,API_KEY_NEW,API_BASE_NEW,API_VERSION_NEW,ENGINE_MODEL_NEW):
     global openai
@@ -52,6 +51,7 @@ def execute_response(pompt, engine = ENGINE_MODEL, max_tokens = 1024, temperatur
         temperature = temperature
     )['choices'][0]['text'].strip().replace("\n", "").replace("\t", "")
 
+
 def converJSON(json_text):
     start = json_text.find("{")
     end = json_text.rfind("}")
@@ -64,8 +64,11 @@ def converJSON(json_text):
             return {}
     return {}
 
-def create_kahoot_prompt(destinacion_vuelo,tematica, n = 10):
-    prompt = f"Devuelveme un JSON vàlido que contenga una lista llamada 'quiz' de {n} objetos con el siguiente formato:\n- 'question': Una pregunta sobre la city de {destinacion_vuelo} y de tematica {tematica}\n- 'a': Una respuesta valida o invalida sobre la pregunta\n- 'b': Otra respuesta valida o invalida sobre la pregunta\n- 'c': Otra respuesta valida o invalida sobre la pregunta\n- 'd': Otra respuesta valida o invalida sobre la pregunta\n- 'respuesta': La letra (a, b, c, d) que contenga la respuesa correcta a 'question'. Solo una puede ser la correcta, las demás serán falsas"
+def create_kahoot_prompt(flight_destination,topic, n = 10):
+    # INGLÉS
+    prompt = f"Return me a valid JSON containing a list called 'quiz' of {n} objects with the following format:\n- 'question': A question about the city of {flight_destination} and subject {topic}: A valid or invalid answer about the question 'b': Another valid or invalid answer on the question 'c': Another valid or invalid answer on the question 'd': Another valid or invalid answer on the question 'answer': The letter (a, b, c, d) that contains the correct answer to 'question'. Only one can be correct, the others will be false."
+    # ESPAÑOL
+    #prompt = f"Devuelveme un JSON vàlido que contenga una lista llamada 'quiz' de {n} objetos con el siguiente formato:\n- 'question': Una pregunta sobre la city de {destinacion_vuelo} y de tematica {tematica}\n- 'a': Una respuesta valida o invalida sobre la pregunta\n- 'b': Otra respuesta valida o invalida sobre la pregunta\n- 'c': Otra respuesta valida o invalida sobre la pregunta\n- 'd': Otra respuesta valida o invalida sobre la pregunta\n- 'respuesta': La letra (a, b, c, d) que contenga la respuesa correcta a 'question'. Solo una puede ser la correcta, las demás serán falsas"
     # prompt = f'dame un conjunto de 10 preguntas de opción múltiple sobre la city de {destinacion_vuelo}. Cada pregunta debe tener una sola respuesta correcta y estar relacionada con {tematica}. Usa la información disponible sobre {destinacion_vuelo} para crear preguntas interesantes y desafiantes para los usuarios, que cada pregunta  se represente de la siguiente estructura en formato json: {{"¿En qué año fue fundada Barcelona?": {{"A": "218 a. C.", "B": "78 d. C.", "C": "100 d. C.", "D": "410 d. C.", "respuesta": "A"}}}} en una misma linea'
     return prompt.replace("\n", "").replace("\t", "").replace("\\", "")
 
@@ -77,10 +80,11 @@ def cargarJson(jsonString):
 
 def setupInfo(city="Barcelona"):
     res = []
-    categories = ['Cultura', 'Lugares Turísticos', 'Historia', 'Gastronomía', 'Arquitectura']
-    print("Extrayendo Datos de "+city)
+    categories = ['Culture', 'Tourist Sites', 'History', 'Gastronomy', 'Architecture']
+    #categories = ['Cultura', 'Lugares Turísticos', 'Historia', 'Gastronomía', 'Arquitectura']
+    print("Extracting data from "+city)
     for i in range(len(categories)):
-        print("Generando preguntas de " + categories[i]+" de "+city)
+        print("Generating questions from " + categories[i]+" from "+city)
         prompt = create_kahoot_prompt(city, categories[i],2)
         ok = True
         while(ok):
@@ -88,6 +92,7 @@ def setupInfo(city="Barcelona"):
             data = converJSON(response)
             if 'quiz' in data:
                 data = data['quiz']
+                print(data)
                 for _ in data:
                     #print([_['question'],_['a'],_['b'],_['c'],_['d'],_['respuesta']])
                     res.append([_['question'],_['a'],_['b'],_['c'],_['d'],_['respuesta']])
@@ -96,12 +101,11 @@ def setupInfo(city="Barcelona"):
                 ok = True;
 
     for dato in res:
-        # procesar cada pregunta
-        pregunta_texto = dato[0]
-        opciones = [dato[1],dato[2],dato[3],dato[4],dato[5]]
-        db.insertKahootQuest(pregunta_texto, opciones)
-    print("Datos de "+city+" Importados")
-
+        # process each question
+        question_text = dato[0]
+        options = [dato[1],dato[2],dato[3],dato[4],dato[5]]
+        db.insertKahootQuest(question_text, options)
+    print("Data from "+city+" imported")
 
 
 app = Flask(__name__)
@@ -115,19 +119,74 @@ def after_request(response):
 @app.route('/')
 def index():
     html="""
-    <form action="/setup" method="post">
-        <label for="city_origen">city_origen</label>
-        <input type="text" name="city_origen" placeholder="city Origen"/><br>
-        <label for="city_destino">city_destino</label>
-        <input type="text" name="city_destino" placeholder="city Destino"/><br>
-        <label for="fecha_origen">fecha_origen</label>
-        <input type="datetime-local" name="fecha_origen" placeholder="Fecha Origen"/><br>
-        <label for="fecha_destino">fecha_destino</label>
-        <input type="datetime-local" name="fecha_destino" placeholder="Fecha Destino"/><br>
+    <style>
+    :root {
+        --clr-charcoal: #333333;
+        --clr-light-gray: #4D4D4D;
+        --clr-off-white: #F3F3F3;
+        --clr-white: #FFFFFF;
+        --clr-yellow: #FFCC00;
+        --ff-primary: 'Nunito', sans-serif;
+    }
+    form{
+    display:flex;
+    flex-direction:column;
+    align-items:center;
+    justify-content:center;
+    margin-top:20px
+}
+label{
+    font-size:18px;
+    margin-bottom:10px;
+    color:var(--clr-yellow);
+    font-weight:bold;
+    font-family:var(--ff-primary);
+}
+input[type=datetime-local],input[type=text]{
+    padding:10px;
+    border-radius:5px;
+    border:none;
+    background-color:#f5f5f5;
+    color:var(--clr-charcoal);
+    font-size:16px;
+    margin-bottom:20px;
+    width:250px;
+    box-shadow:0 0 5px 0 rgba(0,0,0,.2)
+}
+input[type=submit]{
+    padding:10px 20px;
+    border-radius:5px;
+    border:none;
+    background-color:var(--clr-yellow);
+    color:#fff;
+    font-size:18px;
+    cursor:pointer;
+    transition:background-color .3s ease-in-out
+}
+input[type=submit]:hover{
+    background-color:#148f77
+}
+input[type=text],input[type=datetime]{
+    color:var(--clr-off-white) !important;
+}
+input[type=text]:hover,input[type=datetime]:hover{
+    color:var(--clr-yellow) !important;
+}
+
+</style>
+    <center><form action="/setup" method="post" style="padding:20pt;width:30vw;border-radius:15pt;background:var(--clr-charcoal);">
+        <label for="city_origen">Origin city:</label>
+        <input type="text" name="city_origen" placeholder="Origin city" style="border: 1pt solid var(--clr-yellow);background:var(--clr-charcoal);color:var(--clr-off-white);"/><br>
+        <label for="city_destino">Destination city:</label>
+        <input type="text" name="city_destino" placeholder="Destination city" style="border: 1pt solid var(--clr-yellow);background:var(--clr-charcoal);color:var(--clr-off-white);"/><br>
+        <label for="fecha_origen">Departure date and time:</label>
+        <input type="datetime-local" name="fecha_origen" placeholder="Departure date and time" style="border: 1pt solid var(--clr-yellow);background:var(--clr-charcoal);color:var(--clr-off-white);"/><br>
+        <label for="fecha_destino">Arrival date and time:</label>
+        <input type="datetime-local" name="fecha_destino" placeholder="Arrival date and time" style="border: 1pt solid var(--clr-yellow);background:var(--clr-charcoal);color:var(--clr-off-white);"/><br>
         <input type="submit"/>
-    </form>
+    </form></center>
     """
-    return 'Bienvenido a la API de mi aplicación!<br>'+html
+    return 'Welcome to my application API!<br>'+html
 
 
 @app.route('/setup', methods=['POST'])
@@ -162,18 +221,17 @@ def insert_leaderboard():
     seat = rr.form.get('seat')
     pts = rr.form.get('pts')
     nickname = rr.form.get('nickname')
-    db.insertLeaderboardData(seat,pts,nickname)
-
-    return json.dumps({"status": "OK"})
+    try:
+        db.insertLeaderboardData(seat,pts,nickname)
+        return json.dumps({"status": "200"})
+    except:
+        return json.dumps({"status": "400"})
 
 
 # Ruta para insertar preguntas en la tabla 'preguntas' de la base de datos
 @app.route('/leadborard', methods=['GET'])
 def get_leaderborard():
-    db.getLeaderboard()
-    pregunta = request.data['pregunta']
-    opciones = request.data['opciones']
-    return json.dumps({"status": "OK"})
+    return json.dumps(db.getLeaderboard())
 
 # Ruta para obtener preguntas aleatorias de la tabla 'preguntas' de la base de datos
 @app.route('/questions', methods=['GET'])
